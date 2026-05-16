00dab71a22358d18978c5f31da577d1c3e885ff2
mehul yadav <yaduvanshimehul961@gmail.com>
2026-05-16 13:09:32 +0530

    Add files via upload

---
+++ 6may sql.sql
@@ -0,0 +1,42 @@
+-- joins in sql
+
+create database regexjoin;
+
+use regexjoin;
+
+-- Create first table: customers
+CREATE TABLE customers (
+    customer_id INT,
+    customer_name VARCHAR(50),
+    city VARCHAR(50)
+);
+
+-- Insert records into customers
+INSERT INTO customers VALUES
+(1, 'Amit', 'Delhi'),
+(2, 'Priya', 'Mumbai'),
+(3, 'Rahul', 'Pune'),
+(4, 'Sneha', 'Jaipur'),
+(5, 'Karan', 'Chennai');
+
+
+-- Create second table: orders
+CREATE TABLE orders (
+    order_id INT,
+    customer_id INT,
+    product_name VARCHAR(50)
+);
+
+-- Insert records into orders
+-- Duplicate customer_id is present because some customers ordered multiple products
+INSERT INTO orders VALUES
+(101, 1, 'Laptop'),
+(102, 2, 'Mobile'),
+(103, 1, 'Keyboard'),
+(104, 3, 'Mouse'),
+(105, 2, 'Headphones');
+
+select * from customers;
+select * from orders;  
+
+select  customers.customer_id,customers.customer_name, orders.order_id from customers join orders where customers.customer_id = orders.order_id;
